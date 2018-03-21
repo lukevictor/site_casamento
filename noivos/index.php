5733a870a207e759d8f6f77ded7880df7cf5deef
Felipe Braga Carneiro Leão <felipebcleao@gmail.com>
2018-03-20 23:12:46 -0300

    Alterada a página sobre os "noivos".
    - Inseridos cards sobre cada um dos dois
    - Alterados estilos dos headings
    - removido nome da seção na imagem inicial.

--- noivos/index.php
+++ noivos/index.php
@@ -10,16 +10,55 @@
     	<?php include_once("../template/navbar.tpl.php");?>
 
         <div class="topo noivos">
-            <div class="container">
+            <!-- <div class="container">
                 <div class="row">
                     <div class="col-xs-12">
                         <div class="titulo">Os Noivos</div>
                     </div>
                 </div>
-            </div>
+            </div> -->
         </div>
 
     	<div class="container principal">
+
+            <div class="row">
+                <div class="col-md-12">
+                    <h1>Os Noivos <small>/Um pelo outro</small></h1>
+                </div>
+            </div>
+
+            
+
+            <div class="row">
+                <div class="col-md-4 col-md-offset-2">
+                    <div class="frame-conjuge-wrapper">
+                        <div class="frame-conjuge">
+                            <h1>Camilla Azevedo</h1>
+                            <div class="img-avatar" style="background-image: url(../assets/images/camilla.jpg);"></div>
+                            <section>
+                                <p>Mussum Ipsum, cacilds vidis litro abertis. Quem num gosta di mé, boa gentis num é. Delegadis gente finis, bibendum egestas augue arcu ut est. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis. Nec orci ornare consequat</p>
+                            </section>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="frame-conjuge-wrapper">
+                        <div class="frame-conjuge">
+                            <h1>Felipe Leão</h1>
+                            <div class="img-avatar" style="background-image: url(../assets/images/felipe.jpg);"></div>
+                            <section>
+                                <p>Mussum Ipsum, cacilds vidis litro abertis. Quem num gosta di mé, boa gentis num é. Delegadis gente finis, bibendum egestas augue arcu ut est. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis. Nec orci ornare consequat</p>
+                            </section>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-12">
+                    <section>
+                        <img src="../assets/images/double_arrow.png" alt="Separador" class="img-responsive center separador">
+                    </section>
+                </div>
+            </div>
+
 
             <div class="row">
                 <div class="col-md-12">
@@ -28,30 +67,22 @@
             </div>
 
             <div class="row">
-                <div class="col-md-3 hidden-sm hidden-xs">
+                <!-- <div class="col-md-3 hidden-sm hidden-xs">
                     <div class="im1" style="background-image: url(../assets/images/camilla.jpg);">
                         <div class="im_arrows"></div>
                     </div>
-                </div>
-                <div class="col-md-6 col-sm-12 col-xs-12">
+                </div> -->
+                <div class="col-md-12 col-sm-12 col-xs-12">
                     <section>
                         <p>Mussum Ipsum, cacilds vidis litro abertis. Quem num gosta di mé, boa gentis num é. Delegadis gente finis, bibendum egestas augue arcu ut est. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis. Nec orci ornare consequat. Praesent lacinia ultrices consectetur. Sed non ipsum felis. Posuere libero varius. Nullam a nisl ut ante blandit hendrerit. Aenean sit amet nisi. Pra lá , depois divoltis porris, paradis. Todo mundo vê os porris que eu tomo, mas ninguém vê os tombis que eu levo! In elementis mé pra quem é amistosis quis leo.</p>
                         <p>Si u mundo tá muito paradis? Toma um mé que o mundo vai girarzis! Praesent malesuada urna nisi, quis volutpat erat hendrerit non. Nam vulputate dapibus. Mais vale um bebadis conhecidiss, que um alcoolatra anonimis. Tá deprimidis, eu conheço uma cachacis que pode alegrar sua vidis. Manduma pindureta quium dia nois paga.</p>
                     </section>
                 </div>
-                <div class="col-md-3 hidden-sm hidden-xs">
+                <!-- <div class="col-md-3 hidden-sm hidden-xs">
                     <div class="im2" style="background-image: url(../assets/images/felipe.jpg);">
                         <div class="im_arrows"></div>
                     </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-md-12">
-                <section>
-                        <p>Mé faiz elementum girarzis, nisi eros vermeio. Praesent vel viverra nisi. Mauris aliquet nunc non turpis scelerisque, eget. Delegadis gente finis, bibendum egestas augue arcu ut est. Si u mundo tá muito paradis? Toma um mé que o mundo vai girarzis! Aenean aliquam molestie leo, vitae iaculis nisl. Quem num gosta di mim que vai caçá sua turmis! Em pé sem cair, deitado sem dormir, sentado sem cochilar e fazendo pose. Mauris nec dolor in eros commodo tempor. Aenean aliquam molestie leo, vitae iaculis nisl.</p>
-                        <p>Manduma pindureta quium dia nois paga. Quem num gosta di mim que vai caçá sua turmis! Suco de cevadiss, é um leite divinis, qui tem lupuliz, matis, aguis e fermentis. In elementis mé pra quem é amistosis quis leo.</p>
-                    </section>
-                </div>
+                </div> -->
             </div>
             <div class="row">
                 <div class="col-xs-8 col-xs-offset-2">
--- template/estilos_landing.tpl.php
+++ template/estilos_landing.tpl.php
@@ -1,2 +1,2 @@
 <link href="<?= CSS_ROOT; ?>/sticky_footer.css" rel="stylesheet">
-<link href="<?= CSS_ROOT; ?>/landing.min.css?rel=0318bc6c72" rel="stylesheet">
+<link href="<?= CSS_ROOT; ?>/landing.min.css?rel=8510fc2ee4" rel="stylesheet">
