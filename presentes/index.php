fe601e88320ae29cc7c529ac4b296c28924faa44
Felipe Braga Carneiro Leão <felipeleao88@gmail.com>
2018-05-20 16:22:14 -0300

    Adição de link para lista da Fast Shop

--- presentes/index.php
+++ presentes/index.php
@@ -43,14 +43,14 @@
                 <div class="col-md-8">
                     <h1>Fast Shop</h1>
                     <p>Loja de eletrônicos e eletrodomésticos. Caso prefira uma loja física é possível encontrá-las em diversos shoppings na cidade do Rio de Janeiro.</p>
-                    <p><span class="glyphicon glyphicon-new-window" aria-hidden="true"></span>&nbsp;<a target="_blank" href="#">Link direto para a lista</a></p>
+                    <p><span class="glyphicon glyphicon-new-window" aria-hidden="true"></span>&nbsp;<a target="_blank" href="https://listadecasamento.fastshop.com.br/ListaCasamento/ListaCasamentoProdutos.aspx?ID=247643">Link direto para a lista</a></p>
                 </div>
             </div>
 
             <div class="row card-loja">
                 <div class="col-md-4 hidden-xs hidden-sm">
                     <!-- <img src="../assets/images/Etna-Logo.png" class="img-responsive center" /> -->
-                    <img src="../assets/images/logo_tokstok.png" class="img-responsive center" />
+                    <img src="../assets/images/logo_tokstok .png" class="img-responsive center" />
                 </div>
                 <div class="col-md-8">
                     <h1>Tok &amp; Stok</h1>
