5cd7f75a0c20f15c01aa3798f3921c739033de75
Felipe Braga Carneiro Leão <felipeleao88@gmail.com>
2018-06-11 11:24:46 -0300

    Alterado carregamento das fonts para corrigir IE

--- template/estilos_landing.tpl.php
+++ template/estilos_landing.tpl.php
@@ -1,2 +1,2 @@
 <link href="<?= CSS_ROOT; ?>/sticky_footer.css" rel="stylesheet">
-<link href="<?= CSS_ROOT; ?>/landing.min.css?rel=b0bffcccad" rel="stylesheet">
+<link href="<?= CSS_ROOT; ?>/landing.min.css?rel=9e9c34f694" rel="stylesheet">
