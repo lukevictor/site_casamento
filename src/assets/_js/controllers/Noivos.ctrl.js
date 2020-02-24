edda9f58eb0c7b7b8253ee1084a2673de1672982
Lucas <lucas.c.victor@hotmail.com>
2020-02-24 14:56:59 -0300

    Troca de fotos

--- src/assets/_js/controllers/Noivos.ctrl.js
+++ src/assets/_js/controllers/Noivos.ctrl.js
@@ -13,17 +13,6 @@ app.controller('NoivosController', function ($scope, $log) {
         {src: "../assets/images/opt/ensaio/C_F_PreWedding-0044.jpg"},
         {src: "../assets/images/opt/ensaio/C_F_PreWedding-0062.jpg"},
         {src: "../assets/images/opt/ensaio/C_F_PreWedding-0092.jpg", largo: true},
-        {src: "../assets/images/opt/ensaio/C_F_PreWedding-0073.jpg"},
-        {src: "../assets/images/opt/ensaio/C_F_PreWedding-0108.jpg"},
-        {src: "../assets/images/opt/ensaio/C_F_PreWedding-0112.jpg"},
-        {src: "../assets/images/opt/ensaio/C_F_PreWedding-0122.jpg"},
-        {src: "../assets/images/opt/ensaio/C_F_PreWedding-0113.jpg"},
-        {src: "../assets/images/opt/ensaio/C_F_PreWedding-0128.jpg"},
-        {src: "../assets/images/opt/ensaio/C_F_PreWedding-0140.jpg"},
-        {src: "../assets/images/opt/ensaio/C_F_PreWedding-0153.jpg", largo: true},
-        {src: "../assets/images/opt/ensaio/C_F_PreWedding-0166.jpg"},
-        {src: "../assets/images/opt/ensaio/C_F_PreWedding-0179.jpg"},
-        {src: "../assets/images/opt/ensaio/C_F_PreWedding-0189.jpg"}
     ];
 
     /**
